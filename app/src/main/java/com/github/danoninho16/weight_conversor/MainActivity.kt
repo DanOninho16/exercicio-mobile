5e234c7e8686d13644b47b60394d76391e456d29
DanOninho16 <dan22cardoso@gmail.com>
2023-08-21 14:25:34 -0300

    Programa finalizado

--- app/src/main/java/com/github/danoninho16/weight_conversor/MainActivity.kt
+++ app/src/main/java/com/github/danoninho16/weight_conversor/MainActivity.kt
@@ -2,10 +2,28 @@ package com.github.danoninho16.weight_conversor
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.github.danoninho16.weight_conversor.databinding.ActivityMainBinding
+import java.text.NumberFormat
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.calculateButton.setOnClickListener{ calculateConvert()}
+    }
+    fun calculateConvert() {
+        val stringInTextField = binding.converterValue.text.toString()
+        val cost = stringInTextField.toDouble()
+        val selectedId = binding.conversionOption.checkedRadioButtonId
+        val conversionValue = when(selectedId) {
+            R.id.kg_to_pounds -> 2.2
+            else 0.45
+        }
+        var convertedValue = conversionValue * cost
+        val formattedTip = NumberFormat.getCurrencyInstance().format(convertedValue)
+        binding.conversionResult.text = getString(R.string.conversion_amount, formattedTip)
     }
 }
