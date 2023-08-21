d8f7662a12a0bfd055f3fa9dfefbce0e726e6a3a
DanOninho16 <dan22cardoso@gmail.com>
2023-08-21 14:07:29 -0300

    arquivo base (para testes)

---
+++ app/src/main/java/com/github/danoninho16/weight_conversor/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.github.danoninho16.weight_conversor
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
